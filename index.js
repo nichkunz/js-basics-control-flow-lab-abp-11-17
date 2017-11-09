e5cc803ff5f7153b97f0607d16db1dba84f868ef
nicholas kunz <mrnicholaskunz@gmail.com>
2017-11-09 14:20:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ function scuberGreetingForFeet(totalDistance) {
   if (totalDistance <= 400) {
     return 'This one is on me!'
   }
-  else if (total Distance > 2000) && (totalDistance <= 2500) {
-    return 'I will gladly take your thirty bucks.'
-  }
+  // else if (total Distance > 2000) && (totalDistance <= 2500) {
+  //   return 'I will gladly take your thirty bucks.'
+  // }
   else {
     return 'No can do.'
   }

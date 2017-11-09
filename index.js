3972ae55e66d7237ac86cd8c5abe7178b2bc3737
nicholas kunz <mrnicholaskunz@gmail.com>
2017-11-09 15:40:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your code in this file!
+function scuberGreetingForFeet(totalDistance) {
+  if (totalDistance <= 400) {
+    return 'This one is on me!'
+  }
+  else if (totalDistance > 2000 && totalDistance <=2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else {
+    return 'No can do.'
+  }
+}
+
+function ternaryCheckCity(city) {
+return city == 'NYC' ? 'Ok, sounds good.' : 'No go.';
+}
+
+function switchOnCharmFromTip(amount) {
+  switch (amount) {
+    case 'generous' :
+      return 'Thank you so much.';
+    case 'not as generous' :
+      return 'Thank you.';
+    case 'thanks for everything' :
+      return 'Bye.';
+  }
+}

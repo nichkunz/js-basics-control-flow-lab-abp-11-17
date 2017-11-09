602d7904723eca22a849d61a0eaebb87e984517a
nicholas kunz <mrnicholaskunz@gmail.com>
2017-11-09 15:13:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function scuberGreetingForFeet(totalDistance) {
 }
 
 function ternaryCheckCity(city) {
-let city = 'NYC' ? 'Ok, sounds good.' : 'No go.';
+if city = 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 // function ternaryCheckCity(city) {
 //   city = 'NYC' ? "Ok, sounds good." : "No go.";

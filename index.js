fadf0c5e5df7bcc46f6cb8d12a3d36f27c4c2272
nicholas kunz <mrnicholaskunz@gmail.com>
2017-11-09 15:28:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function scuberGreetingForFeet(totalDistance) {
 }
 
 function ternaryCheckCity(city) {
-city == 'NYC' ? 'Ok, sounds good.' : 'No go.';
+return city == 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 // function ternaryCheckCity(city) {
 //   city = 'NYC' ? "Ok, sounds good." : "No go.";
